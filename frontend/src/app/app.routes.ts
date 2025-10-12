4cc64f4b0ea0a271dded7a01d8ec4afd53ba795e
RobbyDevs <robertcruzsm@gmail.com>
2025-10-12 19:40:53 -0400

    Incluído rotas para login e home-adm em 'app.rountes.ts'

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { CadastroCategoria } from './pages/cadastro-categoria/cadastro-categoria';
-
+import {Login} from './pages/login/login';
+import {HomeAdm} from './pages/home-adm/home-adm';
 export const routes: Routes = [
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home',component:Home},
-    {path:'cad',component:CadastroCategoria}
+    {path: 'login',component :Login},
+    {path:'cad',component: CadastroCategoria},
+    {path: 'homeAdm',component: HomeAdm}
 ];
