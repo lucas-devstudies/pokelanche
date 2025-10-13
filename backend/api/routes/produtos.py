bf5e335c1e8fe12e70397f0dd73615e744af4020
Matheus <matheus1204h@gmail.com>
2025-10-13 17:03:54 -0300

    add id

--- backend/api/routes/produtos.py
+++ backend/api/routes/produtos.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-
 @router.get("/produtos/{id}")
 def get_produto_by_id(id: int):
     return {"titulo": "X-Pikachu", "url": "../assets/1.png", "descricao": "Uma explosão de sabores", "valor": 22.99}
